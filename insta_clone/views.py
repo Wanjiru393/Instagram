bc70cf6e95851a4f37d506de6063610684205010
Wanjiru393 <carolciru393@gmail.com>
2022-06-09 15:16:41 +0300

    Created Register Page

--- insta_clone/views.py
+++ insta_clone/views.py
@@ -4,13 +4,16 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as authlogin
 
+from .forms import RegisterForm, AddImageForm, UpdateImageForm,UpdateProfileForm
+from insta_clone.models import Image,Profile,Follow,Likes,Comment
+
 # Create your views here.
 def login(request):
    if request.method == 'POST':
       username = request.POST.get('username')
       password = request.POST.get('password')
       print(username, password)
-      
+
       try:
          user = User.objects.get(username=username, password=password)
 
@@ -25,3 +28,24 @@ def login(request):
          messages.error(request, 'Invalid username or password')
 
    return render(request, 'insta_clone/login.html')
+
+
+def register(request):
+   register_form = RegisterForm()
+   if request.method == 'POST':
+      form = RegisterForm(request.POST)
+      if form.is_valid():
+         user = form.save(commit=False)
+         user.save()
+         profile = Profile.objects.create(username=username , owner=user)
+         profile.save()
+         print(profile)
+         
+      return redirect('login')
+   context = {
+      'form': register_form,
+   }
+
+   return render(request, 'insta_clone/register.html', context)
+
+      
